e81917449fa9c1d98202162d3dd09f83909e2eb9
NgyenKhoi <vunguyenkhoi47@gmail.com>
2026-06-23 22:20:10 +0700

    again

--- foodrescue-be/src/main/java/com/foodrescue/foodrescue_be/config/WebConfig.java
+++ foodrescue-be/src/main/java/com/foodrescue/foodrescue_be/config/WebConfig.java
@@ -3,7 +3,7 @@ package com.foodrescue.foodrescue_be.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,10 +14,11 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
-        stringConverter.setWriteAcceptCharset(false);
-        converters.add(0, stringConverter);
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Configure JSON converter to use UTF-8
+        converters.stream()
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .forEach(converter -> ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8));
     }
 
     @Bean
